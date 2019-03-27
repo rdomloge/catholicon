663aeee5800c54e9cd84a1a3a8128fd050d0d097
Ramsay Domloge <ramsay.domloge@bca.com>
2019-03-27 22:03:14 +0000

    Added to see if tests work once there is a test

---
+++ src/test/java/catholicon/CodeToBeCalledByFunctionTest.java
@@ -0,0 +1,14 @@
+package catholicon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CodeToBeCalledByFunctionTest {
+
+	@Test
+	final void testCreateMessage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
