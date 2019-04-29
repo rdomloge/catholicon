7cc718483725bc13989c6792da4942a4567b5b38
Ramsay Domloge <ramsay.domloge@bca.com>
2019-04-30 00:11:56 +0100

    Fixing URLs again

--- src/main/webapp/static/script/catholicon-common.js
+++ src/main/webapp/static/script/catholicon-common.js
@@ -2,7 +2,7 @@ var myApp = angular.module('app', ['ngRoute', 'ngCookies', 'pascalprecht.transla
 
 var Config = {
 	BASE_URL: '',
-	MS_SEASONS_BASE: 'http://localhost:81/seasons',
-	MS_LEAGUES_BASE: 'http://localhost:82/leagues',
-	MS_RESULTS_BASE: 'http://localhost:83'
+	MS_SEASONS_BASE: window.location.protocol+'://'+window.location.hostname+':81/seasons',
+	MS_LEAGUES_BASE: window.location.protocol+'://'+window.location.hostname+':82/leagues',
+	MS_RESULTS_BASE: window.location.protocol+'://'+window.location.hostname+':83'
 };
