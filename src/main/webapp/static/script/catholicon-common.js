95dfcdcb45d0009929361d10e3f1b30ee89b1320
domloger <ramsay.domloge@bca.com>
2019-10-15 20:18:45 +0100

    Added port number jiggery pokery

--- src/main/webapp/static/script/catholicon-common.js
+++ src/main/webapp/static/script/catholicon-common.js
@@ -2,7 +2,8 @@ var myApp = angular.module('app', ['ngRoute', 'ngCookies', 'pascalprecht.transla
 
 var Config = {
 	BASE_URL: '',
-	MS_SEASONS_BASE: '//' + window.location.hostname + '/seasons',
-	MS_LEAGUES_BASE: '//' + window.location.hostname + '/leagues',
-	MS_RESULTS_BASE: '//' + window.location.hostname
+	
+	MS_SEASONS_BASE: '//' + window.location.hostname + (location.port ? ':'+location.port: '') + '/seasons',
+	MS_LEAGUES_BASE: '//' + window.location.hostname + (location.port ? ':'+location.port: '') + '/leagues',
+	MS_RESULTS_BASE: '//' + window.location.hostname + (location.port ? ':'+location.port: '')
 };
