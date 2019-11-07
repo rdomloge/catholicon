d897b650c01d4727d34074045e0466c06a1ffe38
Ramsay Domloge <ramsay.domloge@bca.com>
2019-11-07 00:16:34 +0000

    Loads of changes - forgotten files

---
+++ src/main/java/catholicon/domain/LightWeightLeagueForSpidering.java
@@ -0,0 +1,16 @@
+package catholicon.domain;
+public class LightWeightLeagueForSpidering {
+		private int leagueTypeId;
+
+		public LightWeightLeagueForSpidering() {
+		}
+
+		public int getLeagueTypeId() {
+			return leagueTypeId;
+		}
+
+		public void setLeagueTypeId(int leagueTypeId) {
+			this.leagueTypeId = leagueTypeId;
+		}
+		
+	}
