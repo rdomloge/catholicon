2ddb5b5fbd4d0985f3f3f6a7aff162a71efcdb48
Ramsay Domloge <rdomloge@gmail.com>
2022-03-03 21:38:33 +0000

    Trying to fix the issues with Selenium tests

--- buildCodeAndCreateNewImagesForBothArchitectures.sh
+++ buildCodeAndCreateNewImagesForBothArchitectures.sh
@@ -3,6 +3,6 @@
 PACKAGE=${PWD##*/}
 echo Building $PACKAGE
 
-mvn clean package && \
+mvn clean package -Dmaven.test.skip=true  && \
 docker buildx build --platform linux/amd64,linux/arm64\
  -t rdomloge/catholicon-multiarch:latest . --push
